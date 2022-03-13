8eb2ed37f165ef7d4092ba04906ab023f29be7d3
HoangTranNe <93782876+HoangTranNe@users.noreply.github.com>
2022-03-13 22:42:35 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,17 @@
-﻿/* Khai báo và khởi tạo một List số nguyên. In ra màn hình
-giá trị thuộc tính Count và Capacity của List*/
-using System;
+﻿using System;
 class program
-{  
+{
     static void Main()
     {
-
-        List<int> btA = new List<int>(10);
-        btA.Add(1);
-        btA.Add(2);
-        btA.Add(3);
-        btA.Add(4);
-        btA.Add(5);
-        btA.Add(6);
-        btA.Add(7);
-        btA.Add(8);
-        btA.Add(9);
-        btA.Add(10);
-
-        btA.RemoveAt(1);
-        btA.RemoveAt(2);
-
-        Console.WriteLine($"Count after add 10: {btA.Count}");
-        Console.WriteLine($"Capacity after add 10: {btA.Capacity}");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            List<int> btb = new List<int>(n);
+            btb.Add(n);
+            btb.Remove(n);
+            Console.WriteLine("Count is :" + btb.Count);
+            Console.WriteLine("Capacity is : " + btb.Capacity);
+        }
         Console.ReadLine();
-
     }
 }
