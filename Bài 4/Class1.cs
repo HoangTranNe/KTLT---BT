be7da3f28c46b150d9a1362af345036a49cdbef0
HoangTranNe <93782876+HoangTranNe@users.noreply.github.com>
2022-03-17 22:52:41 +0700

    Bai 4

---
+++ Bài 4/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace Bài_4;
+
+public class Class1
+{
+    static void sumdong(int[,] a)
+    {
+        int sum = 0;
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                
+            }
+        }
+    }
+}
